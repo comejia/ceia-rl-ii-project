5a79d1004ff7f5c7576905f52bcd1f28b697f6ca
comejia <comejia@teco.com.ar>
2026-06-23 00:10:52 -0300

    feat: reformat code

--- trabajo_practico/robot_arm_env.py
+++ trabajo_practico/robot_arm_env.py
@@ -1,8 +1,6 @@
 import gymnasium as gym
 from gymnasium import spaces
-
 import numpy as np
-
 import pybullet as p
 import pybullet_data
 
@@ -16,11 +14,8 @@ import pybullet_data
 # -----------------
 # 23 valores
 
-#obs_size = 20
-
 
 class RobotArmEnv(gym.Env):
-
     def __init__(self, render=False):
         super().__init__()
 
@@ -34,33 +29,24 @@ class RobotArmEnv(gym.Env):
         else:
             self.client = p.connect(p.DIRECT)
 
-        p.setAdditionalSearchPath(
-            pybullet_data.getDataPath()
-        )
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
         # 7 articulaciones
         self.n_joints = 7
 
         # acciones continuas
         self.action_space = spaces.Box(
-            low=-0.05,
-            high=0.05,
-            shape=(self.n_joints,),
-            dtype=np.float32
+            low=-0.05, high=0.05, shape=(self.n_joints,), dtype=np.float32
         )
 
         # observaciones
         obs_size = 23
 
         self.observation_space = spaces.Box(
-            low=-np.inf,
-            high=np.inf,
-            shape=(obs_size,),
-            dtype=np.float32
+            low=-np.inf, high=np.inf, shape=(obs_size,), dtype=np.float32
         )
 
         self.max_steps = 200
-
 
     def reset(self, seed=None, options=None):
         super().reset(seed=seed)
@@ -71,28 +57,17 @@ class RobotArmEnv(gym.Env):
 
         p.setGravity(0, 0, -9.81)
 
-        self.plane = p.loadURDF(
-            "plane.urdf"
-        )
+        self.plane = p.loadURDF("plane.urdf")
 
-        self.robot = p.loadURDF(
-            "kuka_iiwa/model.urdf",
-            useFixedBase=True
-        )
+        self.robot = p.loadURDF("kuka_iiwa/model.urdf", useFixedBase=True)
 
-        #self.target_pos = np.random.uniform(
+        # self.target_pos = np.random.uniform(
         #    low=[0.4, -0.3, 0.2],
         #    high=[0.7, 0.3, 0.6]
-        #)
-        self.target_pos = np.random.uniform(
-            low=[0.5, -0.1, 0.4],
-            high=[0.6, 0.1, 0.5]
-        )
+        # )
+        self.target_pos = np.random.uniform(low=[0.5, -0.1, 0.4], high=[0.6, 0.1, 0.5])
 
-        self.target = p.loadURDF(
-            "sphere_small.urdf",
-            basePosition=self.target_pos
-        )
+        self.target = p.loadURDF("sphere_small.urdf", basePosition=self.target_pos)
 
         self.previous_distance = self._distance_to_target()
 
@@ -101,15 +76,10 @@ class RobotArmEnv(gym.Env):
         return obs, {}
 
     def _get_obs(self):
-
         obs = []
 
         for i in range(self.n_joints):
-
-            joint_state = p.getJointState(
-                self.robot,
-                i
-            )
+            joint_state = p.getJointState(self.robot, i)
 
             position = joint_state[0]
             velocity = joint_state[1]
@@ -117,10 +87,7 @@ class RobotArmEnv(gym.Env):
             obs.append(position)
             obs.append(velocity)
 
-        link_state = p.getLinkState(
-            self.robot,
-            6
-        )
+        link_state = p.getLinkState(self.robot, 6)
 
         ee_pos = link_state[0]
 
@@ -133,49 +100,27 @@ class RobotArmEnv(gym.Env):
 
         obs.extend(error)
 
-        return np.array(
-            obs,
-            dtype=np.float32
-        )
+        return np.array(obs, dtype=np.float32)
 
     def _get_joint_positions(self):
-
         positions = []
 
         for i in range(self.n_joints):
-
-            joint_state = p.getJointState(
-                self.robot,
-                i
-            )
+            joint_state = p.getJointState(self.robot, i)
 
             positions.append(joint_state[0])
 
-        return np.array(
-            positions,
-            dtype=np.float32
-        )
+        return np.array(positions, dtype=np.float32)
 
     def _get_end_effector_position(self):
+        link_state = p.getLinkState(self.robot, 6)
 
-        link_state = p.getLinkState(
-            self.robot,
-            6
-        )
-
-        return np.array(
-            link_state[0],
-            dtype=np.float32
-        )
+        return np.array(link_state[0], dtype=np.float32)
 
     def _distance_to_target(self):
-
         ee_pos = self._get_end_effector_position()
 
-        return np.linalg.norm(
-            ee_pos - self.target_pos
-        )
-    
+        return np.linalg.norm(ee_pos - self.target_pos)
 
     def _compute_reward_distance(self):
         """Computa el reward basado en la distancia absoluta."""
@@ -183,7 +128,7 @@ class RobotArmEnv(gym.Env):
         distance = self._distance_to_target()
 
         return -distance
-    
+
     def _compute_reward_progress(self):
         """Computa el reward premiando el progreso."""
 
@@ -195,9 +140,7 @@ class RobotArmEnv(gym.Env):
 
         return reward
 
-    
     def _compute_reward_progress_shaping(self):
-
         current_distance = self._distance_to_target()
 
         progress = self.previous_distance - current_distance
@@ -211,29 +154,24 @@ class RobotArmEnv(gym.Env):
         return progress
 
     def apply_action(self, action):
-
         current_positions = self._get_joint_positions()
 
         target_positions = current_positions + action
 
         for joint in range(self.n_joints):
-
             p.setJointMotorControl2(
                 bodyUniqueId=self.robot,
                 jointIndex=joint,
                 controlMode=p.POSITION_CONTROL,
-                targetPosition=float(
-                    target_positions[joint]
-                ),
-                force=500
+                targetPosition=float(target_positions[joint]),
+                force=500,
             )
 
-        #p.stepSimulation()
+        # p.stepSimulation()
         for _ in range(5):
             p.stepSimulation()
 
     def step(self, action):
-
         self.apply_action(action)
 
         self.steps += 1
@@ -253,13 +191,7 @@ class RobotArmEnv(gym.Env):
 
         info = {}
 
-        return (
-            obs,
-            reward,
-            terminated,
-            truncated,
-            info
-        )
+        return (obs, reward, terminated, truncated, info)
 
     def close(self):
         import pybullet as p

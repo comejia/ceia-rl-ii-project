9e1d292dad7c3cf76a599f015c4d6ecd32ef99d8
comejia <comejia@teco.com.ar>
2026-06-23 00:09:55 -0300

    fix test

--- trabajo_practico/test_ppo.py
+++ trabajo_practico/test_ppo.py
@@ -4,40 +4,23 @@ from robot_arm_env import RobotArmEnv
 
 from stable_baselines3.common.monitor import Monitor
 
-env = Monitor(
-    RobotArmEnv(render=False),
-    filename="monitor.csv"
-)
+env = Monitor(RobotArmEnv(render=False), filename="monitor.csv")
 
 
-# model = PPO(
-#     "MlpPolicy",
-#     env,
+# Train model
+model = PPO("MlpPolicy", env, verbose=1, device="cpu", tensorboard_log="./logs")
+
+# # Transfer Learning
+# model = PPO.load(
+#     "<model_saved>",
+#     env=env,
 #     verbose=1,
 #     device="cpu",
 #     tensorboard_log="./logs"
 # )
 
+model.learn(total_timesteps=400_000, tb_log_name="ppo_robot_arm_v2_5cm")
 
-# Transfer Learning
-model = PPO.load(
-    "ppo_robot_arm_v1_5cm",
-    env=env,
-    verbose=1,
-    device="cpu",
-    tensorboard_log="./logs"
-)
-
-model.learn(
-    total_timesteps=400_000,
-    tb_log_name="fine_tune_5cm"
-)
-
-
-# model.learn(
-#     total_timesteps=200_000,
-#     tb_log_name="rew_distance_progress_shaping_error"
-# )
 
 print("Entrenamiento finalizado")
 
